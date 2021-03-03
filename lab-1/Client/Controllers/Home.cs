30c971c674c611960e19ebb13006946faeb0b49c
UlyanYunakh <ulyanyunakh@gmail.com>
2021-03-03 11:36:04 +0300

    add MVC

---
+++ lab-1/Client/Controllers/Home.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

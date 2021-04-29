031d48d0eb33b5b0ed5fb9d777f983429d4288a1
UlyanYunakh <ulyanyunakh@gmail.com>
2021-04-29 21:38:32 +0300

    add client

---
+++ lab-2/Client/Controllers/Home.cs
@@ -0,0 +1,43 @@
+﻿using FormulaLib;
+using Microsoft.AspNetCore.Mvc;
+
+// Ulyan Yunakh, 821704, Lab-2 LOIS
+
+namespace Client.Controllers
+{
+    public class Home : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string formula1, string formula2)
+        {
+            ViewBag.IsFormula1 = false;
+            ViewBag.Formula1 = formula1;
+
+            ViewBag.IsFormula2 = false;
+            ViewBag.Formula2 = formula2;
+            
+            ViewBag.IsCompare = false;
+
+            if (formula1 != null && formula2 != null)
+            {
+                if (!Formula.Check(formula1))
+                    return View();
+                ViewBag.IsFormula1 = true;
+
+                if (!Formula.Check(formula2))
+                    return View();
+                ViewBag.IsFormula2 = true;
+                
+                ViewBag.IsCompare = Formula.Compare(formula1, formula2);
+            }
+
+            return View();
+        }
+    }
+}

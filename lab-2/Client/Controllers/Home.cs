58d3446850a8abcf30ce994b7e1dd3fbfcbb97fa
UlyanYunakh <ulyanyunakh@gmail.com>
2021-05-07 16:26:17 +0300

    bug fix

--- lab-2/Client/Controllers/Home.cs
+++ lab-2/Client/Controllers/Home.cs
@@ -16,26 +16,33 @@ namespace Client.Controllers
         [HttpPost]
         public IActionResult Index(string formula1, string formula2)
         {
-            ViewBag.IsFormula1 = false;
-            ViewBag.Formula1 = formula1;
-
-            ViewBag.IsFormula2 = false;
-            ViewBag.Formula2 = formula2;
-            
-            ViewBag.IsCompare = false;
-
-            if (formula1 != null && formula2 != null)
+            if (formula1 == null)
+            {
+                ViewBag.IsFormula1 = false;
+                return View();
+            }
+            else
             {
                 if (!Formula.Check(formula1))
                     return View();
                 ViewBag.IsFormula1 = true;
+                ViewBag.Formula1 = formula1;
+            }
 
+            if (formula2 == null)
+            {
+                ViewBag.IsFormula2 = false;
+                return View();
+            }
+            else
+            {
                 if (!Formula.Check(formula2))
                     return View();
                 ViewBag.IsFormula2 = true;
-                
-                ViewBag.IsCompare = Formula.Compare(formula1, formula2);
+                ViewBag.Formula2 = formula2;
             }
+
+            ViewBag.IsCompare = Formula.Compare(formula1, formula2);
 
             return View();
         }

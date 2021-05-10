c3d7b7d5ee7eaa7f1bd50460dfedb28f93b92fdf
UlyanYunakh <ulyanyunakh@gmail.com>
2021-05-10 22:27:00 +0300

    finish

---
+++ lab-3/simple.pl
@@ -0,0 +1,22 @@
+travel(e, w).
+travel(w, e). 
+ 
+move([X, X, Коза, Капуста], волк, [Y, Y, Коза, Капуста]) :- travel(X,Y).
+move([X, Волк, X, Капуста], коза, [Y, Волк, Y, Капуста]) :- travel(X,Y).
+move([X, Волк, Коза, X], капуста, [Y, Волк, Коза, Y]) :- travel(X,Y).
+move([X, Волк, Коза, Капуста], ничего, [Y, Волк, Коза, Капуста]) :- travel(X,Y).
+ 
+safe([X, _, X, _]).
+safe([X, X, _, X]).
+
+not_member(_, []) :- !.
+not_member(X, [Head | Tail]) :-
+  X \= Head,
+  not_member(X, Tail).
+
+solve([[e, e, e, e] | _], []).
+solve([State | OtherStates], [Move | OtherMoves]) :- 
+  move(State, Move, NextState),
+  safe(NextState),
+  not_member(NextState, [State | OtherStates]),
+  solve([NextState, State | OtherStates], OtherMoves).

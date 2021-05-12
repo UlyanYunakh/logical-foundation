6dbde8bb8e43f92ac36647cc9b93b3aa6fc9fd6c
Ulyan Yunakh <44743429+UlyanYunakh@users.noreply.github.com>
2021-05-12 11:39:35 +0300

    Lab 3 (#3)
    * start project
    
    * calculate func
    
    * fill matrix with bool value
    
    * remove matrix, complete compare method
    
    * add client
    
    * compare logical const fix
    
    * "currect" compare method
    
    * bug fix
    
    * finish
    
    * add final state
    
    * add task
    
    * add author

---
+++ lab-3/simple.pl
@@ -0,0 +1,28 @@
+# Юнах Ульян, 821704, ЛОИС Лаб-3 Вариант-2
+
+travel(l, r).
+travel(r, l). 
+ 
+move([X, X, Коза, Капуста], волк, [Y, Y, Коза, Капуста]) :- travel(X,Y).
+move([X, Волк, X, Капуста], коза, [Y, Волк, Y, Капуста]) :- travel(X,Y).
+move([X, Волк, Коза, X], капуста, [Y, Волк, Коза, Y]) :- travel(X,Y).
+move([X, Волк, Коза, Капуста], ничего, [Y, Волк, Коза, Капуста]) :- travel(X,Y).
+ 
+safe([X, _, X, _]).
+safe([X, X, _, X]).
+
+not_member(_, []) :- !.
+not_member(X, [Head | Tail]) :-
+  X \= Head,
+  not_member(X, Tail).
+
+solve([State | _], State, []).
+solve([State | OtherStates], FinalState, [Move | OtherMoves]) :- 
+  move(State, Move, NextState),
+  safe(NextState),
+  not_member(NextState, [State | OtherStates]),
+  solve([NextState, State | OtherStates], FinalState, OtherMoves).
+
+
+task(StartState, FinalState, Moves) :-
+  solve([StartState], FinalState, Moves).
